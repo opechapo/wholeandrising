ea2549b870bd79764763f23475447c6ec660de77
opechapo <abolajiopeyemi000@gmail.com>
2026-01-29 14:16:07 +0100

    fix: remove leftover utils/api import from Login.jsx

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,20 @@
-import axios from "axios";
+// import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
+// const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
 
-const api = axios.create({
-  baseURL: API_BASE,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+// const api = axios.create({
+//   baseURL: API_BASE,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
 
-export default api;
+// export default api;
